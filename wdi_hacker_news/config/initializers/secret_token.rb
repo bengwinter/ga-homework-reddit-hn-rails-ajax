a8b8859cdda18c314a7e7e56f22a565f6814d560
Ben Winter <ben.g.winter@gmail.com>
2013-11-07 16:11:33 -0500

    initial commit

---
+++ wdi_hacker_news/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WdiHackerNews::Application.config.secret_key_base = '41db46dbec53925f83b4d04e2226fda1fd0d1f50456bd28612eca7a2d73422fc1b687e71b6f6cc08446b70497fb26a252e05e220b9f24142c959c8ca44fe0a4f'
---
+++ wdi_hacker_news/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WdiHackerNews::Application.config.session_store :cookie_store, key: '_wdi_hacker_news_session'
