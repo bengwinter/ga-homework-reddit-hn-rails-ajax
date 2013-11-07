6f99860c8e25801bc75a6a08ec1b798ad81629d6
Ben Winter <ben.g.winter@gmail.com>
2013-11-07 16:51:25 -0500

    remove dleted files

--- wdi_hacker_news/db/migrate/20131107211750_comments.rb
+++
@@ -1,9 +0,0 @@
-class Comments < ActiveRecord::Migration
-  def change
-    t.body
-    t.integer :up_votes, default: 0
-    t.belongs_to :user
-    t.belongs_to :story
-    t.timestamps
-  end
-end
