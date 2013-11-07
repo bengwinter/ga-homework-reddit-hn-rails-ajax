81a2c0f97305224d3f38a17e5d0b794375ded688
Ben Winter <ben.g.winter@gmail.com>
2013-11-07 17:04:17 -0500

    modified routes:

--- wdi_hacker_news/config/routes.rb
+++ wdi_hacker_news/config/routes.rb
@@ -2,8 +2,12 @@ WdiHackerNews::Application.routes.draw do
 
   root 'stories#index'
   
-  resources :users
+  post 'users' => 'users#create', :as => 'users'
+  get 'delete' => "users#destroy", :as => "delete_user"
+  get "log_out" => "sessions#destroy", :as => "log_out"
+
   resources :sessions
+  
   resources :stories do
     resources :comments
   end
